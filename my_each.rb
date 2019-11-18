2ee27df042ca8ba450816635df175b6ade936ef2
Ingrid sura <sura.ingrid@gmail.com>
2019-11-18 22:57:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,19 @@
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
+def my_each(array)
+ array_length = array.length 
+ #array.length will return an integer. 
+ 
+ loop_count = 1
+ #this will keep track of how many times we've gone through the loop 
+ 
+ #our my_each method will end once we've gone through every element in the array 
+ 
+ while (loop_count <= array_length) do
+
+  #do this
+  
+  yield(array[loop_count - 1])
+  #we need to pass the element in the array that corresponds to the loop that we're on to yield
+  loop_count += 1
+ end
+ array
 end
